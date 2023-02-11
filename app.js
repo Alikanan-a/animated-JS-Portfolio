467b08e0db69794def0d53b9f47cc6a16aabd2eb
alikanan <a.kanan2222@gmail.com>
2023-02-12 00:45:25 +0330

    main

---
+++ app.js
@@ -0,0 +1,7 @@
+var typingEffect = new Typed(".multiText", {
+    strings :['Programer','3D-Designer', 'Web-Developer'],
+    loop: true,
+    typeSpeed :100,
+    backSpeed:80,
+    backDelay:1500
+})
